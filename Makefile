bbf05df01242220213eca3d65a2ac36e58d289ef
zhangyuan <libinzhangyuan@gmail.com>
2014-08-15 15:58:47 +0800

    zhangyuan master finished 500 msec cp then wait 500 msec

--- Makefile
+++ Makefile
@@ -1,7 +1,40 @@
-CFLAGS=-std=c99
+# flags
+CC = gcc
+CFLAGS = -Wall -std=c99
+LFLAGS =
 
+# args
+RELEASE =0
+BITS =
+
+# [args] 生成模式. 0代表debug模式, 1代表release模式. make RELEASE=1.
+ifeq ($(RELEASE),0)
+	# debug
+	CFLAGS += -g
+else
+	release
+	CFLAGS += -static -O3 -DNDEBUG
+	LFLAGS += -static
+endif
+
+# [args] 程序位数. 32代表32位程序, 64代表64位程序, 其他默认. make BITS=32.
+ifeq ($(BITS),32)
+	CFLAGS += -m32
+	LFLAGS += -m32
+else
+	ifeq ($(BITS),64)
+	CFLAGS += -m64
+	LFLAGS += -m64
+else
+	endif
+endif
+
+
+all : low_io_cp
+	
 low_io_cp : low_io_cp.o
+	$(CC) $(LFLAGS) -o $@ $^
 
 low_io_cp.o : low_io_cp.c
-	cc -c low_io_cp.c
+	$(CC) $(CFLAGS) -c $<
 
--- low_io_cp.c
+++ low_io_cp.c
@@ -5,6 +5,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/time.h>
+// 获取程序位数（被编译为多少位的代码）
+int GetProgramBits()
+{
+    return sizeof(int*) * 8;
+}
+
+
+long long current_timestamp() {
+    struct timeval te;
+    gettimeofday(&te, NULL); // get current time
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
+    return milliseconds;
+}
 
 void error_exit(const char* error_text)
 {
@@ -19,13 +33,15 @@ void write_to_file(const int fd_target, const char* read_buf, const size_t len)
 }
 
 // do cp but only for milliseconds then stop
-// return true means file cp doesn't end.   false means all file copied.
+// return true means all file copied.  true means file cp doesn't end.
 bool cp_for_time(const int fd_src, const int fd_target, const int msec)
 {
-    static off_t cur_cp_pos = 0;
+    //static off_t cur_cp_pos = 0;
+
+    long long start_time = current_timestamp();
 
     char read_buf[1024 * 80] = "";
-    while(1)
+    for(;;)
     {
         ssize_t read_ret = read(fd_src, read_buf, sizeof(read_buf));
         if (read_ret == -1)
@@ -34,23 +50,44 @@ bool cp_for_time(const int fd_src, const int fd_target, const int msec)
             //    continue;
             error_exit("read error");
         }
+
+        // reading all done
         if (read_ret == 0)
         {
-            fsync(fd_target);
-            return false;
+            fsync(fd_target); // force writting to file all.
+            return true;
         }
         
         ssize_t len = read_ret;
         write_to_file(fd_target, read_buf, len);
-    }
 
-    return true;
+        long long cur_time = current_timestamp();
+        if ((cur_time - start_time) > msec)
+        {
+            fsync(fd_target);
+            return false;
+        }
+    }
 }
 
 void do_cp(const int fd_src, const int fd_target)
 {
-    while (cp_for_time(fd_src, fd_target, 500))
+    int each_cp_time = 500;
+
+    for(;;)
     {
+        long long start_time = current_timestamp();
+        bool b_all_copied = cp_for_time(fd_src, fd_target, each_cp_time);
+        if (b_all_copied)
+            return;
+        long long end_cur_cp_time = current_timestamp();
+
+        if ((end_cur_cp_time - start_time) > 500)
+            each_cp_time *= 0.8;
+        else
+            each_cp_time *= 1.1;
+        printf("cur_cp_time milliseconds: %ld  sleep: 500  next cp_time:%d\n", (long)(end_cur_cp_time - start_time), each_cp_time);
+
         usleep(500 * 1000);
     }
 }
@@ -60,6 +97,8 @@ void do_cp(const int fd_src, const int fd_target)
 // only support file with full path name.
 int main(int argc, char **argv)
 {
+    printf("bits:\t%d\n", GetProgramBits());
+
     if (argc != 3)
     {
         printf("Wrong parameter\nPlease give source file name and target file name. \n Example: low_io_cp /data/file1 /date/file2\n");
@@ -81,6 +120,9 @@ int main(int argc, char **argv)
         error_exit("Can't open target file");
     }
 
+    const time_t begin_time = time(NULL);
     do_cp(fd_src, fd_target);
+    const time_t end_time = time(NULL);
+    printf("total seconds of copying file: %ld\n", end_time - begin_time);
     exit(0);
 }
